912c807edc560d0cbb5e29202acdf9a9b6ebb24c
Hamish Bultitude <sendanemailtohamish@gmail.com>
2020-01-10 08:32:18 +1000

    INITIAL COMMIT

---
+++ CompareTheBrewDemo/CompareTheBrewDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CompareTheBrewDemo
+//
+//  Created by Hamish Bultitude on 10/1/20.
+//  Copyright © 2020 Hamish Bultitude. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CompareTheBrewDemo/CompareTheBrewDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CompareTheBrewDemo
+//
+//  Created by Hamish Bultitude on 10/1/20.
+//  Copyright © 2020 Hamish Bultitude. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
